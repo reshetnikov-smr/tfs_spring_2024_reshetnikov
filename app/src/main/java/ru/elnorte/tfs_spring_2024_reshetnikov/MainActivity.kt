c56ec7bc123c112166d907a22aab1ebcce3afc0e
elnorte <elnorte@yandex.ru>
2024-03-16 17:47:05 +0400

    init commit

---
+++ app/src/main/java/ru/elnorte/tfs_spring_2024_reshetnikov/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.elnorte.tfs_spring_2024_reshetnikov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
